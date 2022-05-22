916f083ecae6cf8f249e8bf4c58c07044662307a
Andrea Alvarado <a.alvarado1@ufl.edu>
2022-05-22 14:17:01 -0400

    added js greet() 1-3

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code here
+
+ function greet(userName) {
+    var userName = prompt("What is your name?");
+    alert("Hello " + userName + "!");
+    var age = prompt("How old are you?");
+    
+}
